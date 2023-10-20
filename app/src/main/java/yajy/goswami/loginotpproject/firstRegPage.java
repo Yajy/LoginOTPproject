0bc7c1c0236ff67da2de4871d07cee400d960d0d
Yajy Goswami <goswamiyajy@gmail.com>
2023-10-21 01:06:23 +0530

    otp_version1

---
+++ app/src/main/java/yajy/goswami/loginotpproject/firstRegPage.java
@@ -0,0 +1,14 @@
+package yajy.goswami.loginotpproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class firstRegPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_reg_page);
+    }
+}
